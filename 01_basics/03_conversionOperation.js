1f965faabe450ed45cfbcf962077679e625ffdab
BhavayaGoyal <bhavayagoyal89@gmail.com>
2025-06-10 10:11:29 +0000

    Data Tpes Conversion and Comparision

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,56 @@
+let score = "33abc"
+let score1=null;
+let score2=undefined;
+let score3=true;
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valuInNumber = Number(score2);
+// console.log(typeof valuInNumber);
+// console.log(valuInNumber )
+// console.log(typeof score1);
+// console.log( score1);
+// console.log( score2);
+// console.log( score3);
+
+// "33" => 33
+// "33anc" => NaN
+// true =>1 ; false =>0
+
+let isLoggedIn = 1
+let booleanIsloggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn)
+
+// 1 => true 0 => false
+//" " => false
+// "abc" => true
+
+let someNo = 33;
+
+let stringNo = String(someNo);
+// console.log(stringNo)
+// console.log(typeof stringNo)
+
+// ********************* OPERATIONS *********************
+let value = 3;
+let negvalue = -value;
+console.log(negvalue)
+
+let str1 ="hello"
+let str2 =" bhavaya"
+let str3 = str1 + str2
+console.log(str3)
+
+// console.log("1"+ 2)
+// console.log(1+"2")
+// console.log("1"+2+2) not acceptable in big companies due to confusing conversion
+// console.log(1+2+"2") not acceptable in big companies due to confusing conversion 
+
+
+let num1 , num2 , num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
