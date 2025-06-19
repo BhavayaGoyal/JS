e0e779646bd9b329e188495233a8da67a87378b9
BhavayaGoyal <bhavayagoyal89@gmail.com>
2025-06-19 17:40:22 +0000

    Scopes and mini hoisting

--- 02_basics/06_Scopes.js
+++ 02_basics/06_Scopes.js
@@ -4,10 +4,50 @@ if (true) {
     let a = 10
     const b = 20
     // var c = 30
-    console.log("Block Scope: ", a);
+    // console.log("Block Scope: ", a);
 }
 
 
-console.log("Global Scope: ", a);
+//console.log("Global Scope: ", a);
 //console.log(b);
 // console.log(c);
+
+
+function One(){
+    const username = "ABC"
+
+    function two(){
+        const website = "Utube"
+        console.log(username);
+    }
+    //console.log(website);
+
+    two()
+
+}
+//One()
+
+if (true) {
+    const username = "bhavaya"
+    if(username === "bhavaya"){
+        const web = " youtube" 
+        // console.log(username + web);
+    }
+    //console.log(web);
+}
+
+//console.log(username);
+
+
+//+++++++++++++++++++++++++++++++ interesting ++++++++++++++++++++++++++++
+console.log(addone(5))
+
+function  addone(value){
+    return value + 1
+}  
+
+
+console.log(addTwo(6))
+const addTwo = function(num){
+    return num + 2
+}
