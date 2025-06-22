173dc2f465a3d1832f5ceb6976916b39c7782821
BhavayaGoyal <bhavayagoyal89@gmail.com>
2025-06-22 15:49:15 +0000

    While & Do while loop

---
+++ 04_Iterations/02_Iterations.js
@@ -0,0 +1,21 @@
+let index = 0
+
+while (index <= 10) {
+    //console.log(`value of index is ${index}`);
+    index = index+2
+}
+
+
+let arr =['abc', 'xyz', 'ijk']
+let a=0
+while(a < arr.length){
+   // console.log(`value is ${arr[a]}`);
+    a++
+}
+
+
+let score = 11
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
