62388c26733ecb5f4d4c8f2bac7af39e1cc9bc3a
BhavayaGoyal <bhavayagoyal89@gmail.com>
2025-06-29 15:51:44 +0000

    guess the number

--- 4-guessTheNumber/chaiaurcode.js
+++ 4-guessTheNumber/chaiaurcode.js
@@ -1,4 +1,4 @@
-const randomNo = parseInt(Math.random()*100+1);
+let randomNo = parseInt(Math.random()*100+1);
 const submit = document.querySelector('#subt');
 const user = document.querySelector('#guessField')
 const guessSlot=  document.querySelector('.guesses')
@@ -46,30 +46,51 @@ function validateGuess(guess){
 function checkGuess(guess){
     //for printing msg to check whether the number is matched, high, low
     if(guess === randomNo){
-        displayMsg(`You guesses it right`)
+        displayMsg(`You guessed it right 😊`)
         endGame()
     } else if(guess < randomNo){
-        displayMsg(`Number is too low`)
+        displayMsg(`Number is TOOO low`)
     }else if(guess > randomNo){
-        displayMsg(`Number is too high`)
+        displayMsg(`Number is TOOO high`)
     }
 }
 
 function displayMsg(msg){
     //it will interact with DOM
-    user.value = ''
-    guessSlot.innerHTML += `${guess}`
-    numGuess++; 
+    lowOrHi.innerHTML = `<h3>${msg} </h3>`
 }
 
 function displayGuess(guess){
     //
+     user.value = ''
+    guessSlot.innerHTML += `${guess} `;
+    numGuess++; 
+    remaining.innerHTML = `${11-numGuess} `;
 }
 
 function newgame(){
    //
+
+   const newGameBtn = document.querySelector('#newGame')
+   newGameBtn.addEventListener('click', function (event){
+    randomNo = parseInt(Math.random()*100+1);    
+    prevGuess = [];
+    numGuess = 1;
+    guessSlot.innerHTML= '';
+    remaining.innerHTML = `${11-numGuess} `;
+    user.removeAttribute('disabled');
+    startOver.removeChild(p);
+    playGame=true;
+   })
 }
 
 function endGame(){
     //
+    user.value= '';
+    user.setAttribute('disabled','');
+    p.classList.add('button');
+    p.innerHTML = `<h2 id="newGame"> Start new Game</h2>`;
+    startOver.appendChild(p);
+    playGame=false;
+    newgame();
 }
